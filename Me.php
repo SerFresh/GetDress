d4c125e9b2f4697dd0edae637e1fdca61e813010
SerFresh <freshhy75.42@gmail.com>
2024-02-27 09:52:30 +0700

    Create Me.php

---
+++ Me.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Me</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <style>
+        .container {
+            max-width: 1200px;
+            margin: 0 auto;
+            padding-top: 140px;
+            box-sizing: border-box;
+            background-color: #ffffff;
+            justify-content: center;
+            display: block;
+            }
+            
+        .team-member {
+            border-color: #86BA90;
+            margin: 20px;
+            padding: 20px;
+            border-style: solid;
+            border-radius: 30px;
+        }
+            
+        .team-member img {
+            border-radius: 30px;
+            max-width: 100%;
+            height: auto;
+        }
+
+        .team-member .info {
+            background-color: #ffffff;
+            border-radius: 20px;
+        }
+
+        h2 {
+            display: flex;
+        }
+    </style>
+</head>
+
+<body>
+
+    <header>
+        <a class="me" href="Home.php">
+            <i class="bi bi-chevron-left"></i>
+        </a>
+        <h1>About Me</h1>
+    </header>
+
+    <div class="container">
+        <div class="team-member">
+            <img src="icon/AA.png" alt="Member 1">
+            <div class="info">
+                <h2>เบญจาภา คำพราว (Leader)</h2>
+                <p>รหัสนักศึกษา 65010567 นักศึกษาคณะวิศวกรรมศาสตร์ ภาควิชาวิศวกรรมคอมพิวเตอร์ สาขาระบบไอโอที และสารสนเทศชั้นปีที่ 2 สถาบันเทคโนโลยีพระจอมเกล้าเจ้าคุณทหารลาดกระบัง</p>
+                <br>
+                <p>หน้าที่ :  ดูแลภาพรวม, ทำรูปเล่ม, Research</p>
+            </div>
+        </div>
+        <div class="team-member">
+            <img src="icon/TN.png" alt="Member 2">
+            <div class="info">
+                <h2>กัลยรัตน์ ธานีสอน</h2>
+                <p>รหัสนักศึกษา 65010068 นักศึกษาคณะวิศวกรรมศาสตร์ ภาควิชาวิศวกรรมคอมพิวเตอร์ สาขาระบบไอโอที และสารสนเทศชั้นปีที่ 2 สถาบันเทคโนโลยีพระจอมเกล้าเจ้าคุณทหารลาดกระบัง</p>
+                <br>
+                <p>หน้าที่ : Logo design, Shape research, พัฒนา Function check shape</p>
+            </div>
+        </div>
+        <div class="team-member">
+            <img src="icon/MM.png" alt="Member 3">
+            <div class="info">
+                <h2>จิภาดา กิตติพงศ์ธร</h2>
+                <p>รหัสนักศึกษา 65010128 นักศึกษาคณะวิศวกรรมศาสตร์ ภาควิชาวิศวกรรมคอมพิวเตอร์ สาขาระบบไอโอที และสารสนเทศชั้นปีที่ 2 สถาบันเทคโนโลยีพระจอมเกล้าเจ้าคุณทหารลาดกระบัง</p>
+                <br>
+                <p>หน้าที่ : หน้า User profile</p>
+            </div>
+        </div>
+        <div class="team-member">
+            <img src="icon/SF.png" alt="Member 4">
+            <div class="info">
+                <h2>จิรฐิพร รัตนบุตร</h2>
+                <p>รหัสนักศึกษา 65010130 นักศึกษาคณะวิศวกรรมศาสตร์ ภาควิชาวิศวกรรมคอมพิวเตอร์ สาขาระบบไอโอที และสารสนเทศชั้นปีที่ 2 สถาบันเทคโนโลยีพระจอมเกล้าเจ้าคุณทหารลาดกระบัง</p>
+                <br>
+                <p>หน้าที่ : พัฒนาหน้าหลัก (Main) , Function Favorites</p>
+            </div>
+        </div>
+        <div class="team-member">
+            <img src="icon/YY.png" alt="Member 5">
+            <div class="info">
+                <h2>ณภัทร  ธนาคม</h2>
+                <p>รหัสนักศึกษา 65010260 นักศึกษาคณะวิศวกรรมศาสตร์ ภาควิชาวิศวกรรมคอมพิวเตอร์ สาขาระบบไอโอที และสารสนเทศชั้นปีที่ 2 สถาบันเทคโนโลยีพระจอมเกล้าเจ้าคุณทหารลาดกระบัง</p>
+                <br>
+                <p>หน้าที่ : UI design ด้วย Figma</p>
+            </div>
+        </div>
+        <div class="team-member">
+            <img src="icon/NG.png" alt="Member 6">
+            <div class="info">
+                <h2>ภัทรลภา ศรีทันดร</h2>
+                <p>รหัสนักศึกษา 65010821 นักศึกษาคณะวิศวกรรมศาสตร์ ภาควิชาวิศวกรรมคอมพิวเตอร์ สาขาระบบไอโอที และสารสนเทศชั้นปีที่ 2 สถาบันเทคโนโลยีพระจอมเกล้าเจ้าคุณทหารลาดกระบัง</p>
+                <br>
+                <p>หน้าที่ : Project planner , รวม code</p>
+            </div>
+        </div>
+        <div class="team-member">
+            <img src="icon/NM.png" alt="Member 7">
+            <div class="info">
+                <h2>สุขหทัย เรืองรัมย์</h2>
+                <p>รหัสนักศึกษา 6501119 นักศึกษาคณะวิศวกรรมศาสตร์ ภาควิชาวิศวกรรมคอมพิวเตอร์ สาขาระบบไอโอที และสารสนเทศชั้นปีที่ 2 สถาบันเทคโนโลยีพระจอมเกล้าเจ้าคุณทหารลาดกระบัง</p>
+                <br>
+                <p>หน้าที่ : พัฒนาและออกแบบ Function register, login</p>
+            </div>
+        </div>
+        
+    </div>
+</body>
+</html>
