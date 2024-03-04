dc8886df391e41c00fe476d467f40e1bf6f1896d
SerFresh <151444848+SerFresh@users.noreply.github.com>
2024-03-05 00:24:07 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,14 +1,10 @@
 <?php
 // Connect to server and select database.
-define('servername', 'localhost');
-define('username', 'root');
-define('password', '');
-define('dbname', 'product');
+define('servername', 'sql104.infinityfree.com');
+define('username', 'if0_36009817');
+define('password', 'skdjF6kRCxCajJ');
+define('dbname', 'if0_36009817_getdressed');
 
 $objCon = mysqli_connect(servername, username, password,dbname);
-
-if ($objCon->connect_error) {
-    die("Connection failed: " . $objCon->connect_error);
-}
 
 ?>
